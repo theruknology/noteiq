8be6041b7c3b286c21a235dd6c93995b3744322f
Mohammed Ruknuddin <iosrukn123@gmail.com>
2025-12-03 11:43:25 +0400

    init

---
+++ src/main/java/com/example/noteiq/NoteiqApplication.java
@@ -0,0 +1,13 @@
+package com.example.noteiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteiqApplication.class, args);
+	}
+
+}
